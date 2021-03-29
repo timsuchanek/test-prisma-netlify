80467690a587759764c93d137ee6e93f80032196
timsuchanek <tim.suchanek@gmail.com>
2021-03-29 12:51:26 +0200

    fix

--- pages/api/prisma-test.ts
+++ pages/api/prisma-test.ts
@@ -1,12 +1,12 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '../../prisma/generated'
 
 const prisma = new PrismaClient()
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
   try {
-     const data = await prisma.user.findMany()
-     res.status(200).json(data)
+    const data = await prisma.user.findMany()
+    res.status(200).json(data)
   } catch (err) {
     res.status(500).json({ statusCode: 500, message: err.message })
   }
