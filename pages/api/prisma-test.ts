2c6bee120ba7cd618c80bafd27e8d20bb2392d96
timsuchanek <tim.suchanek@gmail.com>
2021-03-29 16:45:12 +0200

    wip2

--- pages/api/prisma-test.ts
+++ pages/api/prisma-test.ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { PrismaClient } from '../../prisma/generated'
+import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 
