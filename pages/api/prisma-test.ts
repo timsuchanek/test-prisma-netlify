afb5cd2d491d3448ba69ac08520c6bffc9ad7536
timsuchanek <tim.suchanek@gmail.com>
2021-03-31 10:55:44 +0200

    iwp

--- pages/api/prisma-test.ts
+++ pages/api/prisma-test.ts
@@ -1,14 +1,15 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '../../prisma/generated'
-
-const prisma = new PrismaClient()
+import globby from 'globby'
 
 const handler = async (_req: NextApiRequest, res: NextApiResponse) => {
+  const files = await globby('**')
   try {
+    const prisma = new PrismaClient()
     const data = await prisma.user.findMany()
     res.status(200).json(data)
   } catch (err) {
-    res.status(500).json({ statusCode: 500, message: err.message })
+    res.status(500).json(files)
   }
 }
 
